9bc96cbaafb3ae10829cdc6895f8772756c0496a
CaIzii <2470159730@qq.com>
2024-12-29 19:04:44 -0600

    Godot Project template: Implement the basic godot desktop client project structure

---
+++ CO_Desktop/Scripts/Modules/Core/Client/ClientHandler.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var window_width
+var window_height
+var window_size
+
+var window_position
+func _ready() -> void:
+	window_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+	window_height = ProjectSettings.get_setting("display/window/size/viewport_height")
+	window_size = Vector2i(window_width,window_height)
+	 # 获取当前屏幕的分辨率和偏移量
+	var screen_index = DisplayServer.window_get_current_screen()
+	var screen_size = DisplayServer.screen_get_size(screen_index)
+	var screen_position = DisplayServer.screen_get_position(screen_index) # 当前屏幕在多显示器中的起始坐标
+	# 计算窗口在屏幕右下角的全局位置
+	window_position = screen_position + screen_size - window_size
+	DisplayServer.window_set_position(window_position)
+	print("Screen Size:", screen_size)
+	print("Window Size:", window_size)
+	print("Window Position:", window_position)
