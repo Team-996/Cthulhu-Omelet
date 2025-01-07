1c747996ac2718908097a381edb669dd19e6b869
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-01-08 01:17:49 +0800

    feat: pixel perfect detection on sprite2D finished (new task: animatedSprite2D...), and button moving basic logic. Ready to refactor CO_Desktop

--- CO_Desktop/Scripts/Modules/Core/Client/ClientHandler.gd
+++ CO_Desktop/Scripts/Modules/Core/Client/ClientHandler.gd
@@ -1,37 +1,47 @@
 extends Node2D
 
-@onready var sprite2D: Sprite2D = $Sprite2D
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite2D: TestSprite = $Sprite2D
+#@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_player_2: AnimationPlayer = $AnimationPlayer2
+@onready var button: Button = $Button
 
 var window_width
 var window_height
 var window_size
 var window_position
 
+
 func _ready() -> void:
 	window_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 	window_height = ProjectSettings.get_setting("display/window/size/viewport_height")
-	window_size = Vector2i(window_width,window_height)
-	 # 获取当前屏幕的分辨率和偏移量
-	var screen_index = DisplayServer.window_get_current_screen()
-	var screen_size = DisplayServer.screen_get_size(screen_index)
-	var screen_position = DisplayServer.screen_get_position(screen_index) # 当前屏幕在多显示器中的起始坐标
+	window_size = Vector2i(window_width, window_height)
+	# 获取当前屏幕的分辨率和偏移量
+	var screen_index: int         = DisplayServer.window_get_current_screen()
+	var screen_size: Vector2i     = DisplayServer.screen_get_size(screen_index)
+	var screen_position: Vector2i = DisplayServer.screen_get_position(screen_index) # 当前屏幕在多显示器中的起始坐标
 	# 计算窗口在屏幕右下角的全局位置
 	window_position = screen_position + screen_size - window_size
 	DisplayServer.window_set_position(window_position)
 	print("Screen Size:", screen_size)
 	print("Window Size:", window_size)
 	print("Window Position:", window_position)
-	
-	
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("quit_game"):
 		get_tree().quit()
 	if event is InputEventMouseButton and event.button_mask == MOUSE_BUTTON_MASK_RIGHT:
-		var local_mouse_pos = DisplayServer.mouse_get_position()
-		if sprite2D.is_mouse_over_sprite(local_mouse_pos, window_position+window_size/2):	
-			printt(local_mouse_pos)
-			print(get_viewport().get_mouse_position())
+		var screen_mouse_pos: Vector2i = DisplayServer.mouse_get_position()
+		if sprite2D.is_mouse_over_sprite(screen_mouse_pos, window_position+window_size/2):
+			print("MOUSE SCREEN POS: ", screen_mouse_pos)
+			print("Mouse is hovering over the visible part of the sprite!")
 		else:
-			#animation_player.play("Run_L")
-			print("Mouse is not over the sprite!") 
+			print("Mouse is not over the sprite!")
+
+	# TODO: pixel perfect detection when animation player is playing
+	if event.is_action_pressed("pet_start_running"):
+		#animation_player.play("Run_L")
+		animation_player_2.play("button_move")
+		
+	#if event.is_action_pressed("pet_stop_running"):
+		#animation_player.pause()
--- CO_Desktop/Scripts/Modules/Pet/DadavidtsengPOCGPetScript.gd
+++ CO_Desktop/Scripts/Modules/Pet/DadavidtsengPOCGPetScript.gd
@@ -1,24 +1,26 @@
 extends Sprite2D
-
 class_name TestSprite
 
 const UNREAL_EXE_PATH: String = "C:/Users/Yu-Wei Tseng/Downloads/BetterDiscord-Windows.exe"
-var unreal_process_id: int = -1
+var unreal_process_id: int    = -1
+
 
 func is_process_alive(process_id: int) -> bool:
-	var output = []
-	var exit_code = OS.execute("tasklist", [], output, true)
+	var output: Array[Variant] = []
+	var exit_code: int         = OS.execute("tasklist", [], output, true)
 	if exit_code == 0:
 		for line in output:
 			if str(process_id) in line:
 				return true
 	return false
 
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			print("Sprite clicked!")
 			open_client()
+
 
 func open_client():
 	if unreal_process_id == -1 or not is_process_alive(unreal_process_id):
@@ -30,45 +32,33 @@ func open_client():
 	else:
 		print("Process is already running with PID:", unreal_process_id)
 
+
 func _exit_tree():
 	if unreal_process_id != -1 and is_process_alive(unreal_process_id):
 		OS.kill(unreal_process_id)
 		print("Process terminated.")
-		
-#func _process(delta: float) -> void:
-	#var local_mouse_pos = DisplayServer.mouse_get_position()
-	#
-	#if is_mouse_over_sprite(local_mouse_pos):
-		#print("Mouse is hovering over the visible part of the sprite!")
-	#else:
-		#print("Mouse is not over the sprite!") 
-		
+
+
 func is_mouse_over_sprite(local_pos: Vector2, window_center_pos: Vector2)->bool:
 	if texture == null:
-		#print("A", )
 		return false
-	
-	#var screen_size = DisplayServer.screen_get_size()
-	
-	var texture_width = texture.get_width()
-	var texture_height = texture.get_height()
-	
+
+	var texture_width: int  = texture.get_width()
+	var texture_height: int = texture.get_height()
+
 	if local_pos.x < window_center_pos.x-texture_width/2.0 or local_pos.y <  window_center_pos.y-texture_height/2.0 or local_pos.x > window_center_pos.x+texture_width/2.0 or local_pos.y > window_center_pos.y+texture_height/2.0:
-		print("B /",local_pos, window_center_pos)
-		#print(texture.get_size())
+		print("B /", local_pos, window_center_pos)
 		return false
-		
-	var image = texture.get_image()
+
+	var image: Image = texture.get_image()
+
 	if image == null:
-		#print("C")
 		return false
-	
-	#local_pos.y = image.get_height()-local_pos.y
-	var mouse_position = get_viewport().get_mouse_position()
-	var pixel_color = image.get_pixelv(mouse_position)
-	print("PIXEL COLOR A: ", pixel_color, " / PIXEL POS:", mouse_position)
-	
+
+	image.decompress()
+
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	var pixel_color: Color      = image.get_pixelv(mouse_position)
+	print("PIXEL COLOR: ", pixel_color, " / PIXEL LOCAL POS:", mouse_position)
+
 	return pixel_color.a != 0
-		
-			
-	
