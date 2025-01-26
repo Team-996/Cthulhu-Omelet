ba3fb15d15037a8b0d64a79c1c0171a9297ca311
CaIzii <2470159730@qq.com>
2025-01-26 14:43:40 -0600

    Core Register: Add Base content register

---
+++ CO_Client/Source/CO_Client/Core/Register/ContentRegister.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ContentRegister.h"
+
+bool UContentRegister::RegisterFromID_Def(FString id, TSubclassOf<UDefinition> definition)
+{
+	return false;
+}
+
+bool UContentRegister::RegisterFromDef(TSubclassOf<UDefinition> definition)
+{
+	return false;
+}
---
+++ CO_Client/Source/CO_Client/Core/Register/ContentRegister.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ContentRegister.generated.h"
+
+class UDefinition;
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class CO_CLIENT_API UContentRegister : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, Category="Register")
+	TMap<FString, TObjectPtr<UDefinition>> RegisterContext;
+
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Register")
+	bool RegisterFromID_Def(FString id, TSubclassOf<UDefinition> definition);
+	UFUNCTION(BlueprintCallable, Category = "Register")
+	bool RegisterFromDef(TSubclassOf<UDefinition> definition);
+};
