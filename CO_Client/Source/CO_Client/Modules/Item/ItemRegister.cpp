f185cc9c9171fd2f09cf949f097b47b0618ee18e
CaIzii <2470159730@qq.com>
2025-01-26 22:45:48 -0600

    ItemRegister: introduce ItemRegister for register item definition instances from DataTable and Standalone definition classes

---
+++ CO_Client/Source/CO_Client/Modules/Item/ItemRegister.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemRegister.h"
+
+#include "IDetailTreeNode.h"
+#include "ItemDefinition.h"
+#include "ItemRowData.h"
+
+bool UItemRegister::RegisterFromDataTable(UDataTable* DataTable)
+{
+	for (auto& Row : DataTable->GetRowMap())
+	{
+		FItemRegisterDataRow*        Item          = (FItemRegisterDataRow*)(Row.Value);
+		TObjectPtr<UItemDefinition>  definition    = nullptr;
+		TSubclassOf<UItemDefinition> templateClass = nullptr;
+		templateClass                              = Item->DefinitionClassTemplate;
+		definition                                 = NewObject<UItemDefinition>(templateClass);
+		/// Read Table row metadata and parse into item definition
+		definition->ParseMetaData(Item->MetaData);
+		/// End of parse
+		if (definition != nullptr)
+		{
+			RegisterContext.Add(Item->ItemID, definition);
+			return true;
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("RegisterFromDataTable: Failed to register item definition instance"));
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool UItemRegister::RegisterFromDef(TSubclassOf<UDefinition> definition)
+{
+	TObjectPtr<UItemDefinition> ItemDefinition = NewObject<UItemDefinition>(definition);
+	if (ItemDefinition == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("RegisterFromDef(): Failed to register item definition instance"));
+		return false;
+	}
+	else
+	{
+		if (RegisterContext.Find(ItemDefinition->id))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("RegisterFromDef(): You try to register the definition with same id"));
+		}
+		RegisterContext.Add(ItemDefinition->id, ItemDefinition);
+		return true;
+	}
+}
+
---
+++ CO_Client/Source/CO_Client/Modules/Item/ItemRegister.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CO_Client/Core/Register/ContentRegister.h"
+#include "UObject/Object.h"
+#include "ItemRegister.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class CO_CLIENT_API UItemRegister : public UContentRegister
+{
+	GENERATED_BODY()
+
+public:
+	/// Set the datatable for the register
+	UPROPERTY(BlueprintReadWrite)
+	TObjectPtr<UDataTable> ItemRegisterDataTable;
+
+public:
+	/// Register items from correspond datatable, user need to set correct datatable
+	/// or else it would fail to register items
+	/// @param DataTable The datatable need to read its rows to register items
+	/// @return whether or not successful register
+	UFUNCTION(BlueprintCallable, Category = "Register")
+	virtual bool RegisterFromDataTable(UDataTable* DataTable) override;
+	UFUNCTION(BlueprintCallable, Category = "Register")
+	virtual bool RegisterFromDef(TSubclassOf<UDefinition> definition) override;
+};
