be828f0506709947f9b57ceb17136f15dadeee20
CaIzii <2470159730@qq.com>
2025-01-26 22:41:45 -0600

    RegistrationSubsystem: add RegistrationSubsystem to hold all definitions instance

---
+++ CO_Client/Source/CO_Client/Core/Register/RegistrationSubsystem.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RegistrationSubsystem.h"
+
+void URegistrationSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
---
+++ CO_Client/Source/CO_Client/Core/Register/RegistrationSubsystem.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "RegistrationSubsystem.generated.h"
+
+class UContentRegister;
+/**
+ * 
+ */
+UCLASS()
+class CO_CLIENT_API URegistrationSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	UPROPERTY(EditAnywhere, Category = "ContentRegister")
+	TArray<TObjectPtr<UContentRegister>> ContentRegisters;
+};
