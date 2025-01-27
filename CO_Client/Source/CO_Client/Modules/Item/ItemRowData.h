d6677f96b1a14c6a8f10a189b65d726ab462865b
CaIzii <2470159730@qq.com>
2025-01-26 22:45:05 -0600

    ItemRowData: add ItemRowData struct for ItemDefinition Datatable

---
+++ CO_Client/Source/CO_Client/Modules/Item/ItemRowData.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ItemRowData.generated.h"
+
+class UItemDefinition;
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FItemRegisterDataRow
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString ItemID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	int32 MaxStackSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	TArray<FString> Descriptions;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	TMap<FString, FString> MetaData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	TSubclassOf<UItemDefinition> DefinitionClassTemplate;
+};
