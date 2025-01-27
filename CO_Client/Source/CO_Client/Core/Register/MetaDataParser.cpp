b1b2db218d9231b978be4dcf5815b34f14661acc
CaIzii <2470159730@qq.com>
2025-01-26 22:42:39 -0600

    Core-Register: add helper interface for all definitions class that parse metadata from datatable sections

---
+++ CO_Client/Source/CO_Client/Core/Register/MetaDataParser.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MetaDataParser.h"
+
+
+// Add default functionality here for any IMetaDataParser functions that are not pure virtual.
