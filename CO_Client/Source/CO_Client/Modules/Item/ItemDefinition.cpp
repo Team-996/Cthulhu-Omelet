8e682e46cf50780bed8489a2d99f13918d65ada5
CaIzii <2470159730@qq.com>
2025-01-26 22:46:27 -0600

    ItemDefinition: introduce ItemDefinition for register and the template for ItemStack

---
+++ CO_Client/Source/CO_Client/Modules/Item/ItemDefinition.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemDefinition.h"
+
+void UItemDefinition::ParseMetaData(const TMap<FString, FString>& MetaData)
+{
+	Super::ParseMetaData(MetaData);
+}
---
+++ CO_Client/Source/CO_Client/Modules/Item/ItemDefinition.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CO_Client/Core/Register/Definition.h"
+#include "UObject/Object.h"
+#include "ItemDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CO_CLIENT_API UItemDefinition : public UDefinition
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
+	TObjectPtr<UTexture2D> itemIcon;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
+	int64 maxStackSize = 999;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
+	TArray<FString> itemDescriptions;
+	virtual void ParseMetaData(const TMap<FString, FString>& MetaData) override;
+
+private:
+	TMap<FString, FString> itemMetaData;
+};
