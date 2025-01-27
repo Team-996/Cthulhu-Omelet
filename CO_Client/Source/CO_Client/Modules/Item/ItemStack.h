73327e7c442e3fcb184f58e5b690dc698ff2a042
CaIzii <2470159730@qq.com>
2025-01-26 22:44:41 -0600

    ItemStack: introduce ItemStack that could probably be created from some builder by ItemDefinition

---
+++ CO_Client/Source/CO_Client/Modules/Item/ItemStack.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ItemStack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CO_CLIENT_API UItemStack : public UObject
+{
+	GENERATED_BODY()
+};
