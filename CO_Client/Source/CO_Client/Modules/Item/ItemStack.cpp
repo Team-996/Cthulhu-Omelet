b03e48f6f4e5ac43906a4f858a7b6782fe357180
CaIzii <2470159730@qq.com>
2025-02-03 02:08:50 -0600

    ItemStack: - add a mechanism that dynamically registers DataComponents based on its definition - add func()

--- CO_Client/Source/CO_Client/Modules/Item/ItemStack.cpp
+++ CO_Client/Source/CO_Client/Modules/Item/ItemStack.cpp
@@ -2,3 +2,78 @@
 
 
 #include "ItemStack.h"
+
+#include "ItemDefinition.h"
+
+UItemStack::UItemStack()
+{
+}
+
+void UItemStack::Init(TObjectPtr<UItemDefinition> inDefinition, int32 inCount)
+{
+	DataComponents.Reserve(5);
+	Definition = inDefinition;
+	Amount     = inCount;
+
+	/// Traverse TSubclassOf<UItemDataComponent> in Definition and dynamically create instances
+	/// After creating instance, the component need read Item meta data to set its own value
+	for (TSubclassOf<UItemDataComponent> ComponentClass : Definition->DefaultDataComponents)
+	{
+		if (ComponentClass)
+		{
+			UItemDataComponent* Component = NewObject<UItemDataComponent>(this, ComponentClass);
+			Component->ParseMetaData(Definition->GetItemMetaData()); // Inject metadata into component
+			DataComponents.Add(Component);
+			Component->Init(this); // Set the owner item stack
+		}
+	}
+}
+
+int32 UItemStack::GetCount() const
+{
+	return Amount;
+}
+
+void UItemStack::SetCount(int32 inCount)
+{
+	Amount = inCount;
+	if (Amount > Definition->maxStackSize)
+	{
+		Amount = Definition->maxStackSize;
+	}
+}
+
+void UItemStack::AddDataComponent(UItemDataComponent* component)
+{
+	for (TObjectPtr<UItemDataComponent> ItemDataComponent : DataComponents)
+	{
+		if (!ItemDataComponent.IsA(component->GetClass()))
+		{
+			DataComponents.Add(component);
+			UE_LOG(LogTemp, Display, TEXT("UItemStack::AddDataComponent -> Successful add component %s"), *component->GetClass()->GetName());
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("UItemStack::AddDataComponent -> Failed to add component %s (Class duplication)"), *component->GetClass()->GetName())
+		}
+	}
+}
+
+FComponentData UItemStack::GetDataComponent(TSubclassOf<UItemDataComponent> type) const
+{
+	FComponentData componentData;
+	for (TObjectPtr<UItemDataComponent> ItemDataComponent : DataComponents)
+	{
+		if (ItemDataComponent.IsA(type.Get()))
+		{
+			componentData.ItemDataComponent      = ItemDataComponent;
+			componentData.ItemDataComponentClass = type;
+		}
+	}
+	return componentData;
+}
+
+TArray<UItemDataComponent*> UItemStack::GetDataComponents() const
+{
+	return DataComponents;
+}
--- CO_Client/Source/CO_Client/Modules/Item/ItemStack.h
+++ CO_Client/Source/CO_Client/Modules/Item/ItemStack.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/ItemDataComponent.h"
 #include "UObject/Object.h"
 #include "ItemStack.generated.h"
 
+class UItemDataComponent;
+class UItemDefinition;
 /**
  * 
  */
@@ -13,4 +16,33 @@ UCLASS()
 class CO_CLIENT_API UItemStack : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	UItemStack();
+
+	void Init(TObjectPtr<UItemDefinition> inDefinition, int32 inCount = 1);
+
+	UFUNCTION(BlueprintCallable, Category = "ItemStack")
+	int32 GetCount() const;
+
+	UFUNCTION(BlueprintCallable, Category = "ItemStack")
+	void SetCount(int32 inCount);
+
+	UFUNCTION(BlueprintCallable, Category = "ItemStack")
+	void AddDataComponent(UItemDataComponent* component);
+
+	UFUNCTION(BlueprintCallable, Category = "ItemStack")
+	FComponentData GetDataComponent(TSubclassOf<UItemDataComponent> type) const;
+	UFUNCTION(BlueprintCallable, Category = "ItemStack")
+	TArray<UItemDataComponent*> GetDataComponents() const;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	int32 Amount = 1;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UItemDataComponent>> DataComponents;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Definition")
+	TObjectPtr<UItemDefinition> Definition;
 };
