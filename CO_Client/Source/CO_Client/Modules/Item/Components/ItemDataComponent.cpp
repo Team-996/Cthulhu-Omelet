cab6952b94a49f3554623818858c999d73befd80
CaIzii <2470159730@qq.com>
2025-02-03 02:11:10 -0600

    Core-Item: introduce Item Data Component that holds the data from ItemMetaData

---
+++ CO_Client/Source/CO_Client/Modules/Item/Components/ItemDataComponent.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemDataComponent.h"
+
+void UItemDataComponent::Init(UItemStack* owner)
+{
+	OwningStack = owner;
+}
+
+void UItemDataComponent::ParseMetaData(const TMap<FString, FString>& MetaData)
+{
+}
---
+++ CO_Client/Source/CO_Client/Modules/Item/Components/ItemDataComponent.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CO_Client/Core/Register/MetaDataParser.h"
+#include "UObject/Object.h"
+#include "ItemDataComponent.generated.h"
+
+class UItemDataComponent;
+
+USTRUCT(BlueprintType)
+struct FComponentData
+{
+	GENERATED_BODY()
+
+public:
+	TObjectPtr<UItemDataComponent>  ItemDataComponent;
+	TSubclassOf<UItemDataComponent> ItemDataComponentClass;
+};
+
+class UItemStack;
+/**
+ * 
+ */
+UCLASS(BlueprintType, Abstract)
+class CO_CLIENT_API UItemDataComponent : public UObject, public IMetaDataParser
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Item Data")
+	virtual void Init(UItemStack* owner);
+	/// Parse Meta Data and set its own field
+	/// @param MetaData The Metadata component need for constructing its own value
+	virtual void ParseMetaData(const TMap<FString, FString>& MetaData) override;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UItemStack> OwningStack;
+};
