ff96cdc776e6ec13823c3066bdbc767e38257f7c
CaIzii <2470159730@qq.com>
2025-02-03 02:10:36 -0600

    Core-Item: introduce a mechanism that storage needed Data components Classes based on MetaData

--- CO_Client/Source/CO_Client/Modules/Item/ItemDefinition.cpp
+++ CO_Client/Source/CO_Client/Modules/Item/ItemDefinition.cpp
@@ -6,4 +6,22 @@
 void UItemDefinition::ParseMetaData(const TMap<FString, FString>& MetaData)
 {
 	Super::ParseMetaData(MetaData);
+	DefaultDataComponents.Empty();
+	// Read meta data and find the key, base on the key to set Default data component class
+	for (const auto& Pair : MetaData)
+	{
+		if (Pair.Key == "Durability")
+		{
+			//DefaultDataComponents.Add(UDurabilityComponent::StaticClass());
+		}
+		else if (Pair.Key == "Enchantment")
+		{
+			//DefaultDataComponents.Add(UEnchantmentComponent::StaticClass());
+		}
+	}
+}
+
+TMap<FString, FString> UItemDefinition::GetItemMetaData() const
+{
+	return itemMetaData;
 }
--- CO_Client/Source/CO_Client/Modules/Item/ItemDefinition.h
+++ CO_Client/Source/CO_Client/Modules/Item/ItemDefinition.h
@@ -7,6 +7,7 @@
 #include "UObject/Object.h"
 #include "ItemDefinition.generated.h"
 
+class UItemDataComponent;
 /**
  * 
  */
@@ -17,14 +18,22 @@ class CO_CLIENT_API UItemDefinition : public UDefinition
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
-	TMap<FString,TObjectPtr<UTexture2D>> itemIcon;
+	TMap<FString, TObjectPtr<UTexture2D>> itemIcon;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
 	FString itemName = "Default Item";
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
 	int64 maxStackSize = 999;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Item)
 	TArray<FString> itemDescriptions = {"This is the default item", "You should not have this item in your container"};
-	virtual void    ParseMetaData(const TMap<FString, FString>& MetaData) override;
+	/** Parse MetaData and convert it to DataComponent type */
+	virtual void ParseMetaData(const TMap<FString, FString>& MetaData) override;
+	/// Get the ItemMetaData TMap
+	UFUNCTION(BlueprintCallable, Category = "Meta")
+	TMap<FString, FString> GetItemMetaData() const;
+
+	/** Stores the Parsed DataComponent type */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	TArray<TSubclassOf<UItemDataComponent>> DefaultDataComponents;
 
 private:
 	TMap<FString, FString> itemMetaData;
