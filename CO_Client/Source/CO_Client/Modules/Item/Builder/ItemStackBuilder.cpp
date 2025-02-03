5459adc40ac52e8d658c025156f2dc99a0f402a9
CaIzii <2470159730@qq.com>
2025-02-03 02:07:46 -0600

    ItemStackBuilder: introduce ItemStackBuilder for build ItemStack

---
+++ CO_Client/Source/CO_Client/Modules/Item/Builder/ItemStackBuilder.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemStackBuilder.h"
+
+#include "CO_Client/Core/Register/RegistrationSubsystem.h"
+#include "CO_Client/Modules/Item/ItemDefinition.h"
+#include "CO_Client/Modules/Item/ItemStack.h"
+
+UItemStackBuilder::UItemStackBuilder()
+{
+}
+
+UItemStackBuilder* UItemStackBuilder::WithDefinition(UItemDefinition* Definition)
+{
+	_definition = Definition;
+	return this;
+}
+
+UItemStackBuilder* UItemStackBuilder::WithNamespaceAndPath(FString NameSpace, FString Path)
+{
+	UItemDefinition* definition = Cast<UItemDefinition>(URegistrationSubsystem::GetRegistrationSubsystem()->GetValue(NameSpace, Path));
+	_definition                 = definition;
+	return this;
+}
+
+UItemStackBuilder* UItemStackBuilder::WithCount(int32 Count)
+{
+	_count = Count;
+	return this;
+}
+
+UItemStackBuilder* UItemStackBuilder::WithDataComponent(UItemDataComponent* Component)
+{
+	_dataComponents.Add(Component);
+	return this;
+}
+
+UItemStack* UItemStackBuilder::Build()
+{
+	if (!_definition)
+	{
+		UE_LOG(LogTemp, Error, TEXT("UItemStackBuilder::Build() -> No Definition set!"));
+	}
+	// Need Outer perhaps
+	UItemStack* itemStack = NewObject<UItemStack>();
+	itemStack->Init(_definition, _count);
+
+	for (TObjectPtr<UItemDataComponent> ItemDataComponent : _dataComponents)
+	{
+		itemStack->AddDataComponent(ItemDataComponent);
+	}
+
+	return itemStack;
+}
---
+++ CO_Client/Source/CO_Client/Modules/Item/Builder/ItemStackBuilder.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ItemStackBuilder.generated.h"
+
+class UItemDataComponent;
+class UItemStack;
+class UItemDefinition;
+/**
+ * 
+ */
+UCLASS()
+class CO_CLIENT_API UItemStackBuilder : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UItemStackBuilder();
+
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	UItemStackBuilder * WithDefinition(UItemDefinition* Definition);
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	UItemStackBuilder * WithNamespaceAndPath(FString NameSpace = FString("Default"), FString Path = FString("Default"));
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	UItemStackBuilder * WithCount(int32 Count);
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	UItemStackBuilder * WithDataComponent(UItemDataComponent* Component);
+
+	UFUNCTION(BlueprintCallable, Category = "Build")
+	UItemStack * Build();
+
+protected:
+	TObjectPtr<UItemDefinition>            _definition = nullptr;
+	int32                                  _count      = 1;
+	TArray<TObjectPtr<UItemDataComponent>> _dataComponents;
+};
